e524a66ff69c8a27489a3de70c0d3ce899a8ec26
Sumit Kumar <sumit@ubuntu.(none)>
2016-04-06 22:32:26 +0530

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   		log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
       remember user
   		redirect_to user
   	else
